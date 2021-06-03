a51100a961d43a4da323f260c04e9b690ac323e9
Giovanni Ribeiro Caldeira <giovanni.rcaldeira@outlook.com>
2021-06-03 17:56:06 -0400

    Add test to project

---
+++ samples/go/src/main_test.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"leadIntegration/src/controllers"
+)
+
+func TestHealthCheck(t *testing.T) {
+
+	req, err := http.NewRequest("GET", "/health-check", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(controllers.HealthCheck)
+
+	handler.ServeHTTP(rr, req)
+
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("handler returned wrong status code: got %v want %v",
+			status, http.StatusOK)
+	}
+
+	expected := `{"message": "OK"}`
+	if rr.Body.String() != expected {
+		t.Errorf("handler returned unexpected body: got %v want %v",
+			rr.Body.String(), expected)
+	}
+}
