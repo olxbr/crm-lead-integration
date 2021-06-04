f5bafb4de6537ae1c395d2f42bd6778ae9523495
Giovanni Ribeiro Caldeira <giovanni.rcaldeira@outlook.com>
2021-06-04 10:23:18 -0400

    Add tests to project

--- samples/go/src/main_test.go
+++ samples/go/src/main_test.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"net/http"
 	"net/http/httptest"
+	"os"
 	"testing"
 
+	"leadIntegration/src/auth"
 	"leadIntegration/src/controllers"
 )
 
@@ -30,4 +34,84 @@ func TestHealthCheck(t *testing.T) {
 		t.Errorf("handler returned unexpected body: got %v want %v",
 			rr.Body.String(), expected)
 	}
+}
+
+func TestRecieveLead(t *testing.T) {
+
+	requestBody, err := json.Marshal(map[string]string{
+		"leadOrigin":      "VivaReal",
+		"timestamp":       "2017-10-23T15:50:30.619Z",
+		"originLeadId":    "59ee0fc6e4b043e1b2a6d863",
+		"originListingId": "87027856",
+		"clientListingId": "a40171",
+		"name":            "Nome Consumidor",
+		"email":           "nome.consumidor@email.com",
+		"ddd":             "11",
+		"phone":           "999999999",
+		"phoneNumber":     "11999999999",
+		"message":         "Olá, tenho interesse neste imóvel. Aguardo o contato. Obrigado."})
+
+	req, err := http.NewRequest("POST", "/leads/lead", bytes.NewBuffer(requestBody))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	req.SetBasicAuth("vivareal", os.Getenv("SECRET_KEY"))
+
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(controllers.RecieveLead)
+
+	handler.ServeHTTP(rr, req)
+
+	user, secretKey, _ := req.BasicAuth()
+
+	if !auth.ValidAuthorization(user, secretKey) {
+		t.Errorf("handler returned wrong authorization")
+	}
+
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("handler returned wrong status code: got %v want %v",
+			status, http.StatusOK)
+	}
+
+	expected := `{"message": "Lead successfully received"}`
+	if rr.Body.String() != expected {
+		t.Errorf("handler returned unexpected body: got %v want %v",
+			rr.Body.String(), expected)
+	}
+}
+
+func TestWrongAuthorization(t *testing.T) {
+
+	requestBody, err := json.Marshal(map[string]string{
+		"leadOrigin":      "VivaReal",
+		"timestamp":       "2017-10-23T15:50:30.619Z",
+		"originLeadId":    "59ee0fc6e4b043e1b2a6d863",
+		"originListingId": "87027856",
+		"clientListingId": "a40171",
+		"name":            "Nome Consumidor",
+		"email":           "nome.consumidor@email.com",
+		"ddd":             "11",
+		"phone":           "999999999",
+		"phoneNumber":     "11999999999",
+		"message":         "Olá, tenho interesse neste imóvel. Aguardo o contato. Obrigado."})
+
+	req, err := http.NewRequest("POST", "/leads/lead", bytes.NewBuffer(requestBody))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	req.SetBasicAuth("user", "password")
+
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(controllers.RecieveLead)
+
+	handler.ServeHTTP(rr, req)
+
+	user, secretKey, _ := req.BasicAuth()
+
+	if auth.ValidAuthorization(user, secretKey) {
+		t.Errorf("handler returned wrong authorization")
+	}
+
 }
